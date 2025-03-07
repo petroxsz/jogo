80ca8f1257b9ca7e191370e231208dbb47ecb8b1
petroxsz <rogerio.souza@escolar.ifrn.edu.br>
2025-03-07 09:14:45 -0300

    player

--- saulo..jogo/Assets/Player.cs
+++ saulo..jogo/Assets/Player.cs
@@ -11,13 +11,15 @@ public class Player : MonoBehaviour
   
 
   private Rigidbody2D _rigidbody2D;
-  private SpriteRenderer  spriteRenderer; 
+  private SpriteRenderer  spriteRenderer;
+  private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        animator = gameObject.GetComponent<Animator>();
     }
 
 
@@ -40,12 +42,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      float ve1X = 0f;
       if(Input.GetKey(KeyCode.LeftArrow))
       {
         gameObject.transform.position += new Vector3(-velocidade*Time.deltaTime,0,0);
         //rigidbody2D.AddForce(new Vector2(-velocidade,0));
         spriteRenderer.flipX = true;
-        Debug.Log("LeftArrow");
+        ve1X = velocidade;
       }
         
 
@@ -54,9 +57,10 @@ public class Player : MonoBehaviour
         gameObject.transform.position += new Vector3(velocidade*Time.deltaTime,0,0);
         //rigidbody2D.AddForce(new Vector2(velocidade,0));
          spriteRenderer.flipX = false;
-         Debug.Log("RightArrow");
+         ve1X = velocidade;
       }
 
+         animator.SetFloat("Speed", ve1X);
         if (Input.GetKeyDown(KeyCode.Space) && noChao == true)
         {
             _rigidbody2D.AddForce(new Vector2(0, 1) * focaPulo,ForceMode2D.Impulse);
